a662cf987a2098ea8832c35ba865a413409f20ca
Moong <moongstory@gmail.com>
2022-11-10 00:35:05 +0900

    예제 코드 하나만 남기고 전부 삭제

--- Exception/Exception.h
+++ Exception/Exception.h
@@ -9,132 +9,18 @@
 
 #include <iostream>
 #include <Windows.h>
-
-// https://github.com/MoongStory/ConvertDataType
-#include "../../ConvertDataType/ConvertDataType/ConvertDataType.h"
+#include <exception>
 
 namespace MOONG
 {
-	class Exception
-	{
-	public:
-	protected:
-	private:
-
-	public:
-		virtual const std::string getReason() = 0;
-	protected:
-	private:
-	};
-
-
-
-	template <typename T> class ExceptionFunctionCallFailed : public Exception
-	{
-	public:
-	protected:
-	private:
-		std::string function_name;
-		T error_code;
-		bool isGetLastError;
-
-	public:
-		ExceptionFunctionCallFailed(const std::string function_name, T error_code, bool isGetLastError = true) : function_name(function_name), error_code(error_code), isGetLastError(isGetLastError)
-		{
-		}
-
-		const std::string getReason()
-		{
-			return "정의되지 않은 자료형. 클래스 템플릿 특수화를 추가해주세요.";
-		}
-	protected:
-	private:
-	};
-
-	template <> class ExceptionFunctionCallFailed<DWORD> : public Exception
-	{
-	public:
-	protected:
-	private:
-		std::string function_name;
-		DWORD error_code;
-		bool isGetLastError;
-
-	public:
-		ExceptionFunctionCallFailed(const std::string function_name, DWORD error_code, bool isGetLastError = true) : function_name(function_name), error_code(error_code), isGetLastError(isGetLastError)
-		{
-		}
-
-		const std::string getReason()
-		{
-			std::string reason = std::string("[") + this->function_name + std::string("] 함수 호출 실패.");
-
-			if (this->isGetLastError == true)
-			{
-				reason += std::string(" GetLastError[");
-			}
-			else
-			{
-				reason += std::string(" ErrorCode[");
-			}
-
-			reason += MOONG::ConvertDataType::dword_to_string(this->error_code) + std::string("]");
-
-			return reason;
-		}
-	protected:
-	private:
-	};
-
-	template <> class ExceptionFunctionCallFailed<int> : public Exception
-	{
-	public:
-	protected:
-	private:
-		std::string function_name;
-		int error_code;
-		bool isGetLastError;
-
-	public:
-		ExceptionFunctionCallFailed(const std::string function_name, int error_code, bool isGetLastError = true) : function_name(function_name), error_code(error_code), isGetLastError(isGetLastError)
-		{
-		}
-
-		const std::string getReason()
-		{
-			std::string reason = std::string("[") + this->function_name + std::string("] 함수 호출 실패.");
-
-			if (this->isGetLastError == true)
-			{
-				reason += std::string(" GetLastError[");
-			}
-			else
-			{
-				reason += std::string(" ErrorCode[");
-			}
-
-			reason += MOONG::ConvertDataType::int_to_string(this->error_code) + std::string("]");
-
-			return reason;
-		}
-	protected:
-	private:
-	};
-
-	class ExceptionBadAlloc : public Exception
-	{
-	public:
-	protected:
-	private:
-
-	public:
-		const std::string getReason()
-		{
-			return "메모리 할당 실패.";
-		}
-	protected:
-	private:
-	};
+	//class ExceptionFunctionCallFailed : public std::exception
+	//{
+	//public:
+	//	const char* what() const noexcept override
+	//	{
+	//		return "함수 호출 실패.";
+	//	}
+	//};
 }
 
 #endif	// _EXCEPTION_H_
--- Exception/Test.cpp
+++ Exception/Test.cpp
@@ -1,30 +1,18 @@
 #include "Exception.h"
 
-void PrintResult(MOONG::Exception& exception);
+void PrintResult(const std::exception& exception);
 
 int main()
 {
-	MOONG::ExceptionFunctionCallFailed<DWORD> exception_1("함수 이름", 123);
-	PrintResult(exception_1);
-
-	MOONG::ExceptionFunctionCallFailed<DWORD> exception_2("함수 이름", 321, false);
-	PrintResult(exception_2);
-
-	MOONG::ExceptionFunctionCallFailed<int> exception_3("함수 이름", 123);
-	PrintResult(exception_3);
-
-	MOONG::ExceptionFunctionCallFailed<char> exception_4("함수 이름", 'a');
-	PrintResult(exception_4);
-
-	MOONG::ExceptionBadAlloc exception_5;
-	PrintResult(exception_5);
+	//MOONG::ExceptionFunctionCallFailed exception_1;
+	//PrintResult(exception_1);
 
 	return 0;
 }
 
-void PrintResult(MOONG::Exception& exception)
+void PrintResult(const std::exception& exception)
 {
-	std::cout << "reason[" << exception.getReason().c_str() << "]" << std::endl;
+	std::cout << "what[" << exception.what() << "]" << std::endl;
 
 	std::cout << std::endl;
 }
